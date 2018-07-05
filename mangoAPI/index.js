63c6fc6c8f53c75e5630b9177d9e2a2c7669a05d
Juan Carlos <h4rry@Juans-MacBook-Pro.local>
2018-07-04 21:17:28 -0500

    add CRUD on mongo

--- mangoAPI/index.js
+++ mangoAPI/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const booksRouter = require('./routes/routes-books')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 var port = 3000;
 const db = mongoose.connect('mongodb://admin:Passw0rd@ds125831.mlab.com:25831/librillos');
 
-
+app.use(bodyParser.json());
 app.use('/book',booksRouter);
 
 app.listen(port, ()=>{
--- mangoAPI/routes/routes-books.js
+++ mangoAPI/routes/routes-books.js
@@ -1,15 +1,65 @@
 const express = require('express');
 const bookRouter = express.Router();
+const bookModel = require('../models/model-books');
 
-bookRouter.get('/',(re1, res, next)=>{
-    //TODO: Enviar todos los libros
-    res.send("todos los libros");
+bookRouter.get('/',(req, res, next)=>{
+    bookModel.find({},(err,result)=>{
+        if(err){
+            console.log('Error getting Book: ', err);
+            res.status(500).send('Internal Server Error');
+        }else{
+            res.json(result);
+        }
+    })
 });
+
+bookRouter.post('/',(req, res, next)=>{
+    const Newbook = req.body;
+    bookModel.create(Newbook, (err,book)=>{
+        if (err){
+            res.status(500).send({err:err});
+        }else{
+            res.json(book);
+        }
+    })
+});
+
+bookRouter.delete('/:id',(req, res, next)=>{
+    const id = req.params.id;
+    bookModel.deleteOne({_id:id}, (err, result)=>{
+        if (err){
+            res.status(500).send({err:err});
+        }else{
+            res.json(result);
+        }
+    })
+});
+
+bookRouter.put('/:id',(req, res, next)=>{
+    const id = req.params.id;
+    const modifBook = req.body;
+
+    bookModel.findByIdAndUpdate(id, modifBook, (err, result)=>{
+        if (err){
+            res.status(500).send({err:err});
+        }else{
+            bookModel.findById(id, (error, book)=>{
+                res.json(book)
+            })
+        }
+    })
+})
 
 bookRouter.get('/:id',(req, res, next)=>{
     const id = req.params.id;
-    //TODO:  Enviar el libr con su id
-    res.send("el id que mandaste es",id);
+    console.log(id)
+    bookModel.findById(id,(err, book)=>{
+        if (err){
+            res.status(500).send({error:err});
+        }else{
+            res.json(book);
+        }
+    })
 });
 
 module.exports = bookRouter;
